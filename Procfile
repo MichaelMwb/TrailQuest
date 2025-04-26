94e6f49416f266953fd5a7468060afa763c8910f
MichaelMwb <Michaelwbell05@hotmail.com>
2025-04-26 17:10:30 -0400

    Secure API key

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn MOVIESSTORE.wsgigit 
+web: gunicorn TRAILQUEST.wsgi
--- TRAILQUEST/settings.py
+++ TRAILQUEST/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-39-(1x#392=6!5fu$nzt+so(e$^el!*yl-u_fb%l&$nqg#*^=i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,11 +78,10 @@ WSGI_APPLICATION = 'TRAILQUEST.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600, ssl_require=True)
 }
 
 
@@ -120,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+import os
 
+# Static files settings
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Whitenoise settings for serving static files
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
