82e78f535cb3c2ec03c777e9443e42b922385c1b
Michael Bell <157560678+MichaelMwb@users.noreply.github.com>
2025-07-07 20:57:11 +0200

    Update settings.py

--- TRAILQUEST/settings.py
+++ TRAILQUEST/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-39-(1x#392=6!5fu$nzt+so(e$^el!*yl-u_fb%l&$nqg#*^=i'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
